1c2b8c82807e47b6e8ea9c5854e9fbae4d00555f
Giri <giriloveindia@gmail.com>
2018-08-23 12:09:44 +0530

    Create sam.cpp

---
+++ sam.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int *ptr1 = arr;
+    int *ptr2 = arr + 5;
+    printf("%d\n",ptr1);
+    printf("%d\n",ptr2);
+    printf("Number of elements between two pointer are: %d.\n", 
+                                (ptr2 - ptr1));
+    printf("Number of bytes between two pointers are: %d",  
+                              (char*)ptr2 - (char*) ptr1);
+    return 0;
+}
